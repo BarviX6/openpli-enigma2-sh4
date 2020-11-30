57addbf6c16f8fc89b0e344f9d8e3cd96fc108dc
Dimitrij <Dima-73@inbox.lv>
2020-11-30 20:17:36 +0200

    [Hotkey] add "Show satellites list" (#2807)

--- lib/python/Screens/Hotkey.py
+++ lib/python/Screens/Hotkey.py
@@ -170,6 +170,7 @@ def getHotkeyFunctions():
 	hotkey.functions.append((_("Show movies"), "Infobar/showMovies", "InfoBar"))
 	hotkey.functions.append((_("Show servicelist or movies"), "Infobar/showServiceListOrMovies", "InfoBar"))
 	hotkey.functions.append((_("Show favourites list"), "Infobar/openFavouritesList", "InfoBar"))
+	hotkey.functions.append((_("Show satellites list"), "Infobar/openSatellitesList", "InfoBar"))
 	hotkey.functions.append((_("History back"), "Infobar/historyBack", "InfoBar"))
 	hotkey.functions.append((_("Show satellites list"), "Infobar/openSatellites", "InfoBar"))
 	hotkey.functions.append((_("History next"), "Infobar/historyNext", "InfoBar"))
